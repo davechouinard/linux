7f85d54e583e5aabf368947f446f53586a145332
davechouinard <gosteveo@gmail.com>
2017-09-27 08:20:36 -0500

    add color scheme

--- dotfiles/copy.sh
+++ dotfiles/copy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 mkdir -p ~/.local/share/xfce4/terminal/colorschemes
 cp xfce4-terminal/colorschemes/*.theme ~/.local/share/xfce4/terminal/colorschemes/
+[[ -d ~/.vim/bundle/vim-colorschemes ]] && cp vim/dracula.vim ~/.vim/bundle/vim-colorschemes/colors/dracula.vim
+[[ -d ~/.vim/bundle/vim-airline-themes ]] && cp vim/airline/dracula.vim ~/.vim/bundle/vim-airline-themes/autoload/airline/themes/dracula.vim
 
 for i in `grep FILE README.md | sed s/FILE//g`
 do
