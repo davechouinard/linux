be20fb990cb99b8002d41c68f2d5d7bfb58b74c4
davechouinard <gosteveo@gmail.com>
2017-10-20 15:23:36 -0500

    Create archlinux2.sh

---
+++ archlinux2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# this script will be run inside the chroot
+# update myhostname and interfaces as desired
+
+ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
+hwclock --systohc
+echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
+locale-gen
+echo 'LANG=en_US.UTF-8' > /etc/locale.conf
+echo 'KEYMAP=dvorak' > /etc/vconsole.conf
+echo 'myhostname' > /etc/hostname
+echo '127.0.1.1	myhostname.localdomain	myhostname' >> /etc/hosts
+
+pacman -S grub
+grub-install /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+systemctl enable dhcpcd@enp0s3.service
+systemctl enable dhcpcd@enp0s8.service
+
+echo 'net.ipv6.conf.all.disable_ipv6 = 1' > /etc/sysctl.d/40-ipv6.conf
+
+passwd
+exit
