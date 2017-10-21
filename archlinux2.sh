f9c9564757fb5c9a655a88b74f7da656e4c5711d
davechouinard <gosteveo@gmail.com>
2017-10-21 15:30:04 -0500

    Update archlinux2.sh

--- archlinux2.sh
+++ archlinux2.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 # this script will be run inside the chroot
+
+##############################################
 # update myhostname and interfaces as desired
+echo 'myhostname' > /etc/hostname
+echo '127.0.1.1	myhostname.localdomain	myhostname' >> /etc/hosts
+systemctl enable dhcpcd@enp0s3.service
+systemctl enable dhcpcd@enp0s8.service
+##############################################
 
 ln -sf /usr/share/zoneinfo/America/Chicago /etc/localtime
 hwclock --systohc
@@ -8,18 +15,36 @@ echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 locale-gen
 echo 'LANG=en_US.UTF-8' > /etc/locale.conf
 echo 'KEYMAP=dvorak' > /etc/vconsole.conf
-echo 'myhostname' > /etc/hostname
-echo '127.0.1.1	myhostname.localdomain	myhostname' >> /etc/hosts
 
 pacman -S --noconfirm grub
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
-systemctl enable dhcpcd@enp0s3.service
-systemctl enable dhcpcd@enp0s8.service
-
 echo 'net.ipv6.conf.all.disable_ipv6 = 1' > /etc/sysctl.d/40-ipv6.conf
 
+echo 'installing docker'
+tee /etc/modules-load.d/loop.conf <<< "loop"
+modprobe loop 
+pacman -S --noconfirm docker
+systemctl start docker
+systemctl enable docker
+
+echo 'installing packages'
+echo 'choose: virtualbox-guest-modules-arch'
+pacman -S sudo xf86-video-fbdev xf86-video-vesa xorg-server xorg-apps xorg-xinit \
+virtualbox-guest-utils i3 dmenu terminator ttf-dejavu terminus-font tmux zsh vim screenfetch
+
+sed -i '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^# //g' /etc/sudoers
+
+systemctl enable vboxservice.service
+systemctl start vboxservice.service
+
+echo 'setting root passwd'
 passwd
-echo 'type exit to exit chroot'
+
+useradd -m -G wheel -s /bin/bash dave
+echo 'setting user passwd'
+passwd dave
+
+echo 'type exit to exit chroot and finish the install'
 exit 0
