b5ef05f0e339e67918a199d3e7fe1c0a88ca289b
davechouinard <gosteveo@gmail.com>
2017-10-21 09:35:50 -0500

    Update archlinux-config.sh

--- archlinux-config.sh
+++ archlinux-config.sh
@@ -11,6 +11,8 @@ pacman -S sudo xf86-video-fbdev xf86-video-vesa xorg-server xorg-apps xorg-xinit
 virtualbox-guest-utils i3 dmenu terminator ttf-dejavu terminus-font tmux zsh vim screenfetch
 #choose: virtualbox-guest-modules-arch
 
+sed -i '/%wheel ALL=(ALL) NOPASSWD: ALL/s/^# //g' /etc/sudoers
+
 systemctl enable vboxservice.service
 systemctl start vboxservice.service
 
