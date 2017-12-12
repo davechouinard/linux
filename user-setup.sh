237094fefba609bbe134049b89a6c8ee1c4b9943
davechouinard <gosteveo@gmail.com>
2017-12-12 14:55:43 -0600

    Update and rename environment-setup.sh to user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# run this last after git and other system packages have been installed
+# run this as normal user, last, after git and other system packages have been installed.
 
 pip install --user tmuxp
 
@@ -11,9 +11,14 @@ git clone git://github.com/zsh-users/zsh-autosuggestions ~/zsh/zsh-autosuggestio
 git clone git://github.com/zsh-users/zsh-completions.git ~/zsh/zsh-completions
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cd ~/github-source/linux/dotfiles; ./copy.sh 
+cd ~/github-source/linux/dotfiles
+./setup.sh
 fc-cache -vf ~/.fonts
 echo "[[ -f ~/.local.environment ]] && . ~/.local.environment" >> ~/.bashrc
 vim +PlugInstall
+read -p ' - What is your github author name? ' git_authorname
+read -p ' - What is your github author email? ' git_authoremail
+sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" .gitconfig.local.example > ~/.gitconfig.local
+echo
 echo 'logout and log back in'
 exit 0
