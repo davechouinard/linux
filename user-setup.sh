0ea3940b6a23ecff7bb6250fc17f62663edf905e
davechouinard <gosteveo@gmail.com>
2017-12-16 15:05:12 -0600

    Update user-setup.sh

--- user-setup.sh
+++ user-setup.sh
@@ -3,7 +3,7 @@
 
 pip install --user tmuxp
 
-git clone https://github.com/davechouinard/linux.git ~/github-source/linux
+git clone https://github.com/davechouinard/linux.git ~/src/github.com/linux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/bhilburn/powerlevel9k.git ~/zsh/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh/zsh-syntax-highlighting
@@ -11,7 +11,7 @@ git clone git://github.com/zsh-users/zsh-autosuggestions ~/zsh/zsh-autosuggestio
 git clone git://github.com/zsh-users/zsh-completions.git ~/zsh/zsh-completions
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-cd ~/github-source/linux/dotfiles
+cd ~/src/github.com/linux/dotfiles
 ./setup.sh
 fc-cache -vf ~/.fonts
 echo "[[ -f ~/.local.environment ]] && . ~/.local.environment" >> ~/.bashrc
@@ -19,6 +19,8 @@ vim +PlugInstall
 read -p ' - What is your github author name? ' git_authorname
 read -p ' - What is your github author email? ' git_authoremail
 sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" .gitconfig.local.example > ~/.gitconfig.local
+echo
+echo 'launch tmux and do C-a + I'
 echo
 echo 'logout and log back in'
 exit 0
