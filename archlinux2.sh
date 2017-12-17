c426d5653c065eaf92fc9cab0b9f60c4722a0646
davechouinard <gosteveo@gmail.com>
2017-12-16 23:35:20 -0600

    Update archlinux2.sh

--- archlinux2.sh
+++ archlinux2.sh
@@ -33,6 +33,7 @@ sleep 5
 pacman -S sudo xf86-video-fbdev xf86-video-vesa xorg-server xorg-apps xorg-xinit virtualbox-guest-utils \
 openssh python python-pip python2 git tmux zsh vim ctags screenfetch wget dnsutils \
 i3 termite terminus-font ttf-inconsolata ttf-dejavu feh dmenu compton parcellite make
+## todo: awesome-terminal-fonts
 
 ln -s /etc/fonts/conf.avail/10-sub-pixel-rgb.conf /etc/fonts/conf.d
 ln -s /etc/fonts/conf.avail/11-lcdfilter-default.conf /etc/fonts/conf.d
