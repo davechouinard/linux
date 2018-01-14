95629649e861224106d0aea818deec0a50a99810
davechouinard <gosteveo@gmail.com>
2018-01-13 22:52:24 -0600

    Update docker-setup.sh

--- dotfiles/docker-setup.sh
+++ dotfiles/docker-setup.sh
@@ -2,12 +2,12 @@
 # ./docker-setup.sh
 
 pip install --user tmuxp
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/bhilburn/powerlevel9k.git ~/zsh/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/zsh/zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-completions.git ~/zsh/zsh-completions
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ./setup.sh
 #vim +PlugInstall
 #read -p ' - What is your github author name? ' git_authorname
