a98267aeb6c40f7ad6fad5ad8636fcb345902f6d
davechouinard <gosteveo@gmail.com>
2018-01-13 23:56:38 -0600

    Update docker-setup.sh

--- dotfiles/docker-setup.sh
+++ dotfiles/docker-setup.sh
@@ -2,19 +2,12 @@
 # ./docker-setup.sh
 
 pip install --user tmuxp
-#git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/bhilburn/powerlevel9k.git ~/zsh/powerlevel9k
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/zsh/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions ~/zsh/zsh-autosuggestions
 git clone git://github.com/zsh-users/zsh-completions.git ~/zsh/zsh-completions
-#curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 ./setup.sh
-#vim +PlugInstall
 #read -p ' - What is your github author name? ' git_authorname
 #read -p ' - What is your github author email? ' git_authoremail
 #sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" .gitconfig.local.example > ~/.gitconfig.local
-#echo
-#echo 'launch tmux and do C-a + I'
-#echo
-#echo 'logout and log back in'
 exit 0
